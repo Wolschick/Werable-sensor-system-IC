3c028493d65f3d72858dd97476d1cbc9492af860
Wolschick <82110756+Wolschick@users.noreply.github.com>
2022-10-27 15:04:58 -0300

    Insert new_I2C_PIN

--- TwoDevices-BNO080-IMU/TwoDevices-BNO080.ino
+++ TwoDevices-BNO080-IMU/TwoDevices-BNO080.ino
@@ -10,6 +10,13 @@
 #define I2C_SDA 4
 #define I2C_SCL 5
 
+//endereco das IMUs
+#define IMU1_ADD 0x4B
+#define IMU2_ADD 0x4A
+
+//define o tipo de valores que sera feito o TARE
+const uint8_t quat_report = 0x05;
+
 TwoWire I2CIMUs = TwoWire(0); //cria objeto do tipo TwoWire
 
 #include "Biblioteca_IMU_quat.h"
@@ -31,10 +38,22 @@ void setup() {
 
   I2CIMUs.begin(I2C_SDA, I2C_SCL); //inicializa o objeto com as portas definidas de SDA e SCL
   I2CIMUs.setClock(400000);
-  
+
   //define endereco das IMUs
-  IMUs[0].begin_loop(0x4B, &I2CIMUs);
-  IMUs[1].begin_loop(0x4A, &I2CIMUs);
+  IMUs[0].begin_loop(IMU1_ADD, &I2CIMUs);
+  IMUs[1].begin_loop(IMU2_ADD, &I2CIMUs);
+
+  //aplicando TARE
+  int cont = 0;
+  while (cont < 10) {
+    Serial.print("Setting orientation *****");
+    Serial.print("\n");
+    TARE(IMU1_ADD);
+    delay(200);
+    TARE(IMU2_ADD);
+    delay(200);
+    cont++;
+  }
 
   //calculo de tempo de execucao
   _t[0] = micros();
@@ -83,7 +102,22 @@ double * calc_angle(double *p, double *q) {
   euler_angles[0] = atan2(2 * (Cw * Cxyz[0] + Cxyz[1] * Cxyz[2]), 1 - (2 * ((Cxyz[0] * Cxyz[0]) + (Cxyz[1] * Cxyz[1])))); //theta
   euler_angles[1] = asin(2 * ((Cw * Cxyz[1]) - (Cxyz[2] * Cxyz[0]))); //phi
   euler_angles[2] = atan2(2 * (Cw * Cxyz[2] + Cxyz[0] * Cxyz[1]), 1 - (2 * ((Cxyz[1] * Cxyz[1]) + (Cxyz[2] * Cxyz[2])))); //psi
-  
+
   return euler_angles;
 
+}
+
+void TARE(uint8_t BNO_ADDRESS) {
+  static uint8_t RV;               // rotation vector used to tare defined in quat_report
+  if (quat_report == 0x05) RV = 0x00;
+  if (quat_report == 0x08) RV = 0x01;
+  if (quat_report == 0x09) RV = 0x02;
+  uint8_t tare_now[16] = {16, 0, 2, 0, 0xF2, 0, 0x03, 0, 0x07, RV, 0, 0, 0, 0, 0, 0}; //0x07 means all axes 0x04 = Z axis only; based on rotation vector
+  uint8_t tare_persist[16] = {16, 0, 2, 0, 0xF2, 0, 0x03, 0x01, RV, 0, 0, 0, 0, 0, 0, 0};
+  Wire.beginTransmission(BNO_ADDRESS);
+  Wire.write(tare_now, sizeof(tare_now));
+  //Wire.write(tare_persist, sizeof(tare_persist));                                  // uncomment  for tare persist;
+  Wire.endTransmission();
+
+  return;
 }
