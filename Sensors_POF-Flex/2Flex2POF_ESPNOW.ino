aff895e08fea735d6636ee2540d3dd7a713d0ef0
Wolschick <82110756+Wolschick@users.noreply.github.com>
2023-11-17 17:29:28 -0300

    Update 2Flex2POF_ESPNOW.ino
    Add two POF sensors

--- Sensors_POF-Flex/2Flex2POF_ESPNOW.ino
+++ Sensors_POF-Flex/2Flex2POF_ESPNOW.ino
@@ -76,8 +76,8 @@ BLA::Matrix<6> KalmanFilterData = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 BLA::Matrix<6, 6> I6 = {unit, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, unit, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, unit, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, unit, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, unit, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, unit};
 
 float data[6];
-int fail_data2 = 0;
 int fail_data1 = 0;
+int fail_data2 = 0;
 
 // ************
 
@@ -93,11 +93,13 @@ const byte dataReadyPin_2 = 15;
 
 const byte POF_01 = 39;
 const byte POF_02 = 36;
-int POF_fint = 4; 
+const byte POF_fint_A = 32;
+const byte POF_fint_B = 35;
 
 int countdownMS; //variable to count time
 
-int POF_fint_value = 0;
+int POF_fint_value_A = 0;
+int POF_fint_value_B = 0;
 
 //Variables to send with ESPNOW
 typedef struct struct_message {
@@ -107,7 +109,8 @@ typedef struct struct_message {
   float NOW_S2_Xflex;
   float NOW_S2_Yflex;
   float NOW_S2_POF;
-  float NOW_FINT_POF;
+  float NOW_FINT_POF_A;
+  float NOW_FINT_POF_B;
 } struct_message;
 
 // Create a struct_message called myData
@@ -129,14 +132,15 @@ void setup() {
 
   countdownMS = Watchdog.enable(1000); //watchdog timer
 
+  Wire.begin();
+
   // Configure pinmode
   pinMode(dataReadyPin_1, INPUT);
   pinMode(dataReadyPin_2, INPUT);
   pinMode(POF_01, INPUT);
   pinMode(POF_02, INPUT);
-  pinMode(POF_fint, INPUT);
-
-  Wire.begin();
+  pinMode(POF_fint_A, INPUT);
+  pinMode(POF_fint_B, INPUT);
 
   //Setup first sensor - look for it at the default address of 0x13 = 19
   if (myFlexSensor1.begin(45) == false)
@@ -167,7 +171,7 @@ void setup() {
   esp_now_register_send_cb(OnDataSent);
 
   // Register peer
-  memcpy(peerInfo.peer_addr, broadcastAddress, 7);
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
 
@@ -181,6 +185,9 @@ void setup() {
 void loop() {
 
   // ****** Recieving and filter data ******
+
+  POF_fint_value_A = analogRead(POF_fint_A);
+  POF_fint_value_B = analogRead(POF_fint_B);
 
   //Read flex sensor
   if (myFlexSensor1.available() == true) {
@@ -214,7 +221,6 @@ void loop() {
   // Read POF sensors
   pof1_lpfilt  = low_pass_param_1 * pof1_ant + low_pass_param_2 * analogRead(POF_01); // low-pass filter
   pof2_lpfilt  = low_pass_param_1 * pof2_ant + low_pass_param_2 * analogRead(POF_02); // low-pass filter
-  POF_fint_value = analogRead(POF_fint);
 
   data[4] = pof1_lpfilt;
   data[5] = pof2_lpfilt;
@@ -242,15 +248,16 @@ void loop() {
 
   // Send filtered data by ESPNOW
   // Left Sensors
-  myData.NOW_S1_Xflex = (Xk_est(0)); 
+  myData.NOW_S1_Xflex = (Xk_est(0));
   myData.NOW_S1_Yflex = (Xk_est(1));
   myData.NOW_S1_POF   = (Xk_est(2));
   // Right sensors
-  myData.NOW_S2_Xflex = (Xk_est(3)); 
+  myData.NOW_S2_Xflex = (Xk_est(3));
   myData.NOW_S2_Yflex = (Xk_est(4));
   myData.NOW_S2_POF   = (Xk_est(5));
   // New sensor
-  myData.NOW_FINT_POF = POF_fint_value;
+  myData.NOW_FINT_POF_A = POF_fint_value_A;
+  myData.NOW_FINT_POF_B = POF_fint_value_B;
 
   // Send message via ESP-NOW
   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
@@ -275,12 +282,13 @@ void loop() {
     Serial.print("\t");
     Serial.print(Xk_est(5));
     Serial.print("\t");
-    Serial.print(POF_fint_value);
+    Serial.print(POF_fint_value_A);
+    Serial.print("\t");
+    Serial.print(POF_fint_value_B);
     Serial.print("\t");
   }
-
   Serial.print('\n');
 
   countdownMS = Watchdog.enable(1000);
-  delay(4);
+  delay(10);
 }
